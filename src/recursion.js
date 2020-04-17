8813bee9e33325b15f30d5adb72772120ee0c184
rebeccaquey <rebecca_quey@yahoo.com>
2020-04-17 15:22:31 -0700

    Add completed tests for Sum Integers in Array

--- src/recursion.js
+++ src/recursion.js
@@ -33,6 +33,16 @@ var sum = function (array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function (array) {
+  var copyOfArr = array.slice().flat(Infinity);
+  // console.log(copyOfArr);
+  var totalSum = copyOfArr[0];
+  if (array.length === 0) {
+    return 0;
+  } else {
+    copyOfArr.shift();
+    totalSum += arraySum(copyOfArr);
+  }
+  return totalSum;
 };
 
 // 4. Check if a number is even.
